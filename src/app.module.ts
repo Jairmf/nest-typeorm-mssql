5e1ceffb43e1245504bc5b79881466ac44aae6d8
Jair Masgo <jairmf_23@hotmail.com>
2023-11-15 13:22:12 -0500

    Add: Add TypeORM and MSSQL dependencies, configure TypeORM connection, and import Users and Posts modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsService } from './posts/posts.service';
+import { PostsController } from './posts/posts.controller';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'sa',
+      password: '123456Ab',
+      database: 'nestdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      extra: {
+        trustServerCertificate: true,
+      },
+    }),
+    UsersModule,
+    PostsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Profile } from './profile.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User, Profile])
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService] // Exportar servicios consumidos por otro módulo
+})
+export class UsersModule {}
